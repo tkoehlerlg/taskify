5bdbf6121c682e3525110806953e660b0381ccec
Torben Köhler <me@torben-koehler.de>
2024-04-11 01:03:36 +0200

    set up leading page

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,11 +1,3 @@
 import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server'
 
 export const GET = handleAuth()
-
-/*
-import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
-
-<LoginLink>Sign in</LoginLink>
-
-<RegisterLink>Sign up</RegisterLink>
- */
---
+++ src/app/todos/page.tsx
@@ -0,0 +1,3 @@
+export default function TodosPage() {
+    return <p>Todos</p>
+}

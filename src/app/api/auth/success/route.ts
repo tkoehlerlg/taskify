74b42d8fb7b95f334c11036cd5d94dbaa9b3b9f4
Torben Köhler <me@torben-koehler.de>
2024-04-12 02:51:57 +0200

    Revert "test"
    This reverts commit 251ef0cc257b1977c7fca3048e920503dd53992d.

--- src/app/api/auth/success/route.ts
+++ src/app/api/auth/success/route.ts
@@ -1,36 +1,36 @@
-// import { prismaWrite } from '@/utils/prisma'
-// import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
-// import { NextResponse } from 'next/server'
-// import { env } from '@/utils/env'
-//
-// export const GET = async () => {
-//     const { getUser, isAuthenticated } = getKindeServerSession()
-//     try {
-//         const user = await getUser()
-//     } catch (e) {
-//         console.log(e)
-//     }
-//
-//     const user = await getUser()
-//
-//     if (!user || !user.id)
-//         throw new Error('something went wrong with authentication ' + user)
-//
-//     let dbUser = await prismaWrite.user.findUnique({
-//         where: { kindeId: user.id },
-//     })
-//
-//     if (!dbUser) {
-//         await prismaWrite.user.create({
-//             data: {
-//                 kindeId: user.id,
-//                 firstName: user.given_name ?? '',
-//                 lastName: user.family_name ?? '',
-//                 email: user.email ?? '',
-//             },
-//         })
-//         return NextResponse.redirect(env.INTERN_POST_REGISTER_REDIRECT_URL)
-//     }
-//
-//     return NextResponse.redirect(env.INTERN_POST_LOGIN_REDIRECT_URL)
-// }
+import { prismaWrite } from '@/utils/prisma'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import { NextResponse } from 'next/server'
+import { env } from '@/utils/env'
+
+export const GET = async () => {
+    const { getUser, isAuthenticated } = getKindeServerSession()
+    try {
+        const user = await getUser()
+    } catch (e) {
+        console.log(e)
+    }
+
+    const user = await getUser()
+
+    if (!user || !user.id)
+        throw new Error('something went wrong with authentication ' + user)
+
+    let dbUser = await prismaWrite.user.findUnique({
+        where: { kindeId: user.id },
+    })
+
+    if (!dbUser) {
+        await prismaWrite.user.create({
+            data: {
+                kindeId: user.id,
+                firstName: user.given_name ?? '',
+                lastName: user.family_name ?? '',
+                email: user.email ?? '',
+            },
+        })
+        return NextResponse.redirect(env.INTERN_POST_REGISTER_REDIRECT_URL)
+    }
+
+    return NextResponse.redirect(env.INTERN_POST_LOGIN_REDIRECT_URL)
+}
